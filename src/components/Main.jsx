013a4b57e2d975d14809353c06fb9247f557ea0e
Ryan Flanagan <flanagan.ryan@gmail.com>
2020-10-30 16:30:57 -0400

    added styling for App.css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-// import DataArea from "../DataArea/index.js";
+// import React from "react";
+// // import DataArea from "../DataArea/index.js";
 
-function Main (){
-    return (
-        <>
-        <DataArea />
-        </>
-    );
-};
+// function Main (){
+//     return (
+//         <>
+//         <DataArea />
+//         </>
+//     );
+// };
 
-export default Main;
+// export default Main;
