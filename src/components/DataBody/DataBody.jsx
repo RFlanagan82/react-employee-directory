e0ae279404fea764738e828a853792339898345f
Ryan Flanagan <flanagan.ryan@gmail.com>
2020-10-31 13:06:00 -0400

    added comments to DataBody.jsx

--- src/components/DataBody/DataBody.jsx
+++ src/components/DataBody/DataBody.jsx
@@ -6,22 +6,26 @@ import DataAreaContext from "../../utils/DataAreaContext"
 const DataBody = () => {
     const context = useContext(DataAreaContext);
 
-    // Create function to compute Date of Birth
+    // Create function to compute Date of Birth as MM-DD-YYYY
     function formatDate(date){
         const dateArray = date.split("-");
         const year = dateArray[0];
         const month = dateArray[1];
+        //grab day array without the timestamp data starting at 'T' in the API value.
         const dayArray = dateArray[2].split("T");
         const day = dayArray[0];
         const formattedDate =[month, day, year].join("-");
         return formattedDate;
     }
+
+    //Bring in current employee list with filter set to 0 and no undefined items shown.
+    //Map over needed table elements out of the  API to display on my app table.
     return (
         <tbody>
         {context.employeeState.filteredEmps[0] !== undefined && context.employeeState.filteredEmps[0].name !== undefined ? 
-        (context.employeeState.filteredEmps.map(({ login, name, picture, phone, email, dob }) => {
+        (context.employeeState.filteredEmps.map(({ id, name, picture, phone, email, dob }) => {
             return (
-              <tr key={login.uuid}>
+              <tr key={id.uuid}>
                 <td data-th="Image" className="align-middle">
                   <img
                     src={picture.medium}
